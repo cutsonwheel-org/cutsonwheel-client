ff9ea1310c85fabceb20a8247f672df03c853da8
Randy rebucas <rebucasrandy1986@gmail.com>
2020-02-24 15:32:07 +0800

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,22 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', redirectTo: 't', pathMatch: 'full' },
   {
-    path: '',
+    path: 'intro',
+    loadChildren: () => import('./intro/intro.module').then( m => m.IntroPageModule)
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
+  },
+  {
+    path: 't',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'pro',
+    loadChildren: () => import('./pro/pro.module').then( m => m.ProPageModule)
   }
 ];
 @NgModule({
